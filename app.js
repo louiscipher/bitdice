d18b9304aedede727c5826699be5a96de0aa58c6
louiscipher <heathhoulahan@gmail.com>
2017-07-10 01:06:09 +1000

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Lf2bigUAAAAAB6_3gHWuS_Gi3gnbbVkRL63wNLt',  // <----- EDIT ME!
-  redirect_uri: 'https://github.com/louiscipher/louiscipher.github.io',
+  redirect_uri: 'https://github.com/louiscipher/bitdice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
