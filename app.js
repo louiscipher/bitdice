7f10b98b5dba55898cb2f72c506ec476df0337a8
louiscipher <heathhoulahan@gmail.com>
2017-07-09 15:33:50 +1000

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,12 @@
 
 var config = {
   // - Your app's id on moneypot.com
-  app_id: 18,                             // <----------------------------- EDIT ME!
+  app_id: 2915,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'Bitdice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6LdHbCgUAAAAADwxJ6dUOU3jxeRH6WInfFP199-j',  // <----- EDIT ME!
   redirect_uri: 'https://untitled-dice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
