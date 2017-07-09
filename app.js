47c3071ee8c0b580cf977ddb7b1370bb01faac54
louiscipher <heathhoulahan@gmail.com>
2017-07-09 23:09:12 +1000

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'Bitdicey',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '66LdFcSgUAAAAAG-8s-LJSEitC43Xxq1GgD4kWVsE',  // <----- EDIT ME!
   redirect_uri: 'https://untitled-dice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
